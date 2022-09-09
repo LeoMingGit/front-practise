d3bd8fc16b1c66887ee41c91555e7841bd685fe0
Administrator <123>
2022-09-09 11:24:04 +0800

    lx

---
+++ hello-vue3/src/App.vue
@@ -0,0 +1,14 @@
+
+<script>
+  export default {
+    data() {
+      return {
+        message: 'Hello   Vue3!'
+      }
+    }
+  }
+  </script>
+  
+  <template>
+    <h1>{{ message }}</h1>
+  </template>
---
+++ hello-vue3/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+// import the root component App from a single-file component.
+import App from './App.vue'
+
+import './assets/main.css'
+
+createApp(App).mount('#myapp')
